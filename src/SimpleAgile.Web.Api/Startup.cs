6adea50e06a2177c4eaa8e7d460c3e8e7070c9ec
Pratap Bhaskar <Pratap.Bhaskar@harman.com>
2017-02-08 11:15:39 +0530

    initial version - skeleton application

--- src/SimpleAgile.Web.Api/Startup.cs
+++ src/SimpleAgile.Web.Api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace SimpleAgile
+namespace SimpleAgile.Web.Api
 {
     public class Startup
     {
